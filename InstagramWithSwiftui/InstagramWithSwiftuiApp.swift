f1e0401ceefdf095a68ea2508c85355a70427758
Balakrishnan23962 <balakrishnan5523962@gmail.com>
2023-05-15 11:10:34 +0530

    Initial Commit

---
+++ InstagramWithSwiftui/InstagramWithSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramWithSwiftuiApp.swift
+//  InstagramWithSwiftui
+//
+//  Created by Gasc_Internship1 on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramWithSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
